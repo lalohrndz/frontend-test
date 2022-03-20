e758800bb7ca6f4822a8e9191d535219611aa22d
Eduardo Pastor <edo.pstr@gmail.com>
2022-03-20 03:03:15 -0600

    data fetched, css 40% ready and html 90 % ready

---
+++ data.js
@@ -0,0 +1,36 @@
+fetch('products.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(
+        (json)=>{
+            const cards = document.getElementById("cards")
+
+            for (const data in json) {
+                json[data].forEach(product => {
+                    console.log(product);
+
+                    let price = new Intl.NumberFormat('en-IN').format(product.price)
+                    cards.innerHTML = cards.innerHTML + 
+                    '<div class="card">'+
+                        '<div class="card-content">'+
+                            '<div class="img-card">'+
+                                '<img src="img/'+product.img+'" alt="'+product.name+'">'+
+                            '</div>'+
+                            '<div>'+
+                                '<h4>'+product.name+'</h4>'+
+                                '<p>'+product.description+'</p>'+
+                            '</div>'+
+                            '<div class="card-action">'+
+                                '<p>$'+price+'</p>'+
+                                '<a href="#" class="btn-agregar">AGREGAR</a>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'
+                });
+            }
+        }
+        )
+        .catch(function(err){
+            console.log(err);
+        });
